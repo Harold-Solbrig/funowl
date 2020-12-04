b0549a60baa3c630709eaae9fa8af43f0b7f43ad
hsolbrig <solbrig@jhu.edu>
2020-12-04 15:19:54 -0600

    Remove 3.7 unit tests
    This doesn't work with Python 3.7, so we're starting at 3.8...

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
+import sys
+from logging import warn
 
 from setuptools import setup
+
+if sys.version_info < (3, 8, 0):
+    warn(f"FunOwl needs python 3.8 or later.  Current version: {sys.version_info}")
 
 setup(
     setup_requires=['pbr'],
