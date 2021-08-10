dc507739005b75e7fafd0fbdf39a7d83377830b5
hsolbrig <solbrig@jhu.edu>
2021-08-10 14:51:21 -0500

    Enable SNOMED test by adding __init__.py to directory
    Then immediately disable test because it takes too long ;-)

--- tests/test_ontologies/test_snomed.py
+++ tests/test_ontologies/test_snomed.py
@@ -5,13 +5,16 @@ import unittest
 from funowl.converters.functional_converter import to_python
 from tests import datadir
 
+SKIP_SNOMED_TEST = True
+
 
 class SnomedTestCase(unittest.TestCase):
+    @unittest.skipIf(SKIP_SNOMED_TEST, "This test takes a long time...")
     def test_snomed(self):
         start_time = time.time()
         doc = to_python(os.path.join(datadir, 'ontology-after-conversion.owl'))
         print("--- %s seconds ---" % (time.time() - start_time))
-        self.assertTrue(False, "Implement Me")
+
 
 if __name__ == '__main__':
     unittest.main()
